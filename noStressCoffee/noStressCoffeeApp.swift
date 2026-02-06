5a8ad80174ae3bb6fd50b96cc24a8a700b409d96
James Choi <codejc04@gmail.com>
2026-02-06 18:20:34 -0500

    Initial Commit

---
+++ noStressCoffee/noStressCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  noStressCoffeeApp.swift
+//  noStressCoffee
+//
+//  Created by James Choi on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct noStressCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
